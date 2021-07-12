066c146ef13a5f5b951a4e60a7a1c78e6d98f83c
likunyan <lky216@gmail.com>
2021-07-12 09:57:26 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
     'max_age' => 0,
 
